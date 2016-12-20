d888cf5ae94b5554cf0ba19603c7e8d308d57800
John Holdsworth <github@johnholdsworth.com>
2016-12-20 13:04:44 +0000

    Initial commit

---
+++ java_util.h
@@ -0,0 +1,19 @@
+//
+//  java_util.h
+//  java_util
+//
+//  Created by John Holdsworth on 13/07/2016.
+//  Copyright (c) 2016 John Holdsworth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for java_util.
+FOUNDATION_EXPORT double java_utilVersionNumber;
+
+//! Project version string for java_util.
+FOUNDATION_EXPORT const unsigned char java_utilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <java_util/PublicHeader.h>
+
+
