df3ce94a1f81610c38a8953a73cce98e226769e6
John Holdsworth <github@johnholdsworth.com>
2017-08-06 15:42:38 +0100

    Updated for Version 2

--- Package.swift
+++ Package.swift
@@ -11,6 +11,6 @@ import PackageDescription
 let package = Package(
     name: "java_util",
     dependencies: [
-        .Package(url: "https://github.com/SwiftJava/java_lang.git", versions: Version(1,0,0)..<Version(2,0,0)),
+        .Package(url: "https://github.com/SwiftJava/java_lang.git", versions: Version(2,0,0)..<Version(3,0,0)),
         ]
 )
